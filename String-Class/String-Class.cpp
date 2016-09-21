d89d4721bfb00b8a7d3eb94606c832ae39c1ecd8
MaxGuidry <mguidry7@outlook.com>
2016-09-21 17:02:40 -0500

    test

--- String-Class/String-Class.cpp
+++ String-Class/String-Class.cpp
@@ -25,6 +25,7 @@ int main()
 	std::cout << "Prepend: " <<	firstString.prepend("Front -> ") << "\n\n";
 	std::cout << "Lowercase: " << firstString.ToLower() << "\n\n";
 	std::cout << "Uppercase: " << firstString.ToUpper() << "\n\n";
+	std::cout << ";JLKSD;JKLSDFKL;JDFSLJK;" << std::endl;
 	system("pause");
 	return 0;
 }
