c770227057d1fc3d4a9e18bd11eb39e917cdf65a
Donray Williams <w.donray@yahoo.com>
2016-09-22 12:53:41 -0500

    First commit

--- String-Class/String-Class.cpp
+++ String-Class/String-Class.cpp
@@ -3,28 +3,7 @@
 
 int main()
 {
-	char First[255];
-	char Second[255];
-	std::cin.getline(First, 255);
-	std::cin.getline(Second, 255);
-	MyString firstString = MyString(First);
-	MyString secondString = MyString(Second);
-	int index = 3;
-	std::cout << "\nLength of first: " << firstString.getLength() << "\n\n"; 
-	std::cout << "Length of second: " << secondString.getLength() << "\n\n";
-	std::cout << "At the Index 3: " << firstString.accessIndex(index) << "\n\n";
-	std::cout << "Strings Equal? "; 
-	(firstString.compareStrings(secondString)==1)? std::cout << "True\n\n":std::cout<<"False\n\n";
-	std::cout << "Constant C Style String: " << firstString.constantCStyleString() << "\n\n";
-	std::cout << "Is sub string found: ";
-	(firstString.subString("ee") == 1) ? std::cout << "True\n\n" : std::cout << "False\n\n";
-	std::cout << "Is sub string found starting at index " << index << ": ";
-	(firstString.subStringAtIndex(index, "ee")==1) ? std::cout << "True\n\n":std::cout<<"False\n\n";
-	std::cout << "Replace: " << firstString.replace("a", "A") << "\n\n";
-	std::cout << "Append: " << firstString.append(" <- Back") << "\n\n";
-	std::cout << "Prepend: " <<	firstString.prepend("Front -> ") << "\n\n";
-	std::cout << "Lowercase: " << firstString.ToLower() << "\n\n";
-	std::cout << "Uppercase: " << firstString.ToUpper() << "\n\n";
+
 	system("pause");
 	return 0;
 }
